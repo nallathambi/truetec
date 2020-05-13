ecd855a25fa59c289ddbd2cd0a06ba4ca582a2c1
Nallathambi <nallathambi.a98@gmail.com>
2020-05-13 12:35:41 +0530

    fst commit
    first commit of the code

---
+++ src/main/java/com/interview/trutec/TruetecApplication.java
@@ -0,0 +1,13 @@
+package com.interview.trutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruetecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruetecApplication.class, args);
+	}
+
+}
